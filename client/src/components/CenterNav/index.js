bcbf8f1108db2f182dd399c49cff954497d2ac39
Abigail Limpioso <alimpioso@gmail.com>
2020-09-08 18:31:40 -0400

    removing part for presenting

--- client/src/components/CenterNav/index.js
+++ client/src/components/CenterNav/index.js
@@ -7,7 +7,7 @@ function CenterNav(props) {
       <div className="col-9"></div>
       
       {props.isLoggedIn
-        ? <div className="col-lg-5 col-md-10"><ul className="uk-nav  nav-items"><li><a href="/saved-jobs">Saved Jobs</a></li><li><a href="/my-account">My Account</a></li><li><a href="/log-out">Logout</a></li></ul></div>
+        ? <div className="col-lg-5 col-md-10"><ul className="uk-nav  nav-items"><li><a href="/saved-jobs">Saved Jobs</a></li><li><a href="/my-account">My Account</a></li></ul></div>
         : <div className="col-lg-5 col-md-10"><ul className="uk-nav  nav-items"><li><a href="/login">Register/Login</a></li></ul></div>
       }
 
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,10 +7,10 @@ function Nav(props) {
     <nav className="navbar navbar-expand-lg row">
       <a className="devseekers-logo col-8" href="/"><span className="dev-part">Dev</span>Seekers</a>
       <ul className="uk-nav col-4 nav-items">
-          {props.isLoggedIn
-            ? <div><li><a href="/saved-jobs">Saved Jobs</a></li><li><a href="/my-account">My Account</a></li></div>
-            : <div><li><a href="/login">Register/Login</a></li></div>
-          }
+        {props.isLoggedIn
+          ? <div><li><a href="/saved-jobs">Saved Jobs</a></li><li><a href="/my-account">My Account</a></li></div>
+          : <div><li><a href="/login">Register/Login</a></li></div>
+        }
       </ul>
     </nav>
   );
